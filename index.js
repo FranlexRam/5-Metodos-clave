75e090955d0c51b4049fa0b8f1945cd1047baeca
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-01-09 22:08:51 -0400

    Aprendiendo .find() en arrays.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const CORRECTO = "Franlex";
+const OPCIONES = [  {nombre: "franlex", saludo: "Hola"},
+                    {nombre: "rosa", saludo: "Adios"},
+                    {nombre: "martin", saludo: "Buen día"},
+                    {nombre: "ana", saludo: "Saludos"},
+                    {nombre: "marc", saludo: "Que tal??"},
+                    {nombre: "ramon", saludo: "Bien..."},
+                    {nombre: "john", saludo: "Hi"},
+                    {nombre: "jose", saludo: "Buenas"},
+                    {nombre: "mary", saludo: "Hello"},
+                    {nombre: "lauren", saludo: "Bonito dia!"}];
+                    
 document.querySelector(".boton").addEventListener("click", analizar);
 
 function analizar() {
     const NOMBRE = document.querySelector("#nombre").value.trim();
     //Cuando sea igual al valor correcto "Franlex"
-    (NOMBRE===CORRECTO)
-    ? escribir("Hola")
-    : escribir("Chao")
+    const OBJETO =  OPCIONES.find(valor => valor.nombre===NOMBRE)
+    console.log(OBJETO);
+
+    escribir(OBJETO.saludo);
 
 }
 
