000767a34c8d8c2435fe4df629cf7409a2ef2983
Franlex Ramirez <franlex.ramirez@gmail.com>
2025-01-09 21:27:46 -0400

    Funcion analizar

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ document.querySelector(".boton").addEventListener("click", analizar);
 function analizar() {
     const NOMBRE = document.querySelector("#nombre").value.trim();
     //Cuando sea igual al valor correcto "Franlex"
-    
+    (NOMBRE===CORRECTO)
+    ? escribir("Hola")
+    : escribir("Chao")
+
 }
 
 function escribir(valor) {
